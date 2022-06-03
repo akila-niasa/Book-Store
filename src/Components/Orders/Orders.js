622220a3d7cd198d6f7639ee242bb60e58b39a14
akila-niasa <akilaniasa@gmail.com>
2022-06-04 00:23:47 +0600

    finish

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -6,43 +6,43 @@ const Orders = () => {
     useEffect(() => {
         const url = 'https://peaceful-inlet-55752.herokuapp.com/orders';
         fetch(url)
-        .then(res =>res.json())
-        .then(data =>setOrder(data))
+            .then(res => res.json())
+            .then(data => setOrder(data))
     }, []);
     return (
-        <div className = "container">
+        <div className="container">
             <Header></Header>
-        <div className = "row">
-            <div className = "col-md-12">
-            <table className="table mb-5">
-        <thead>
-            <tr>
-                <th scope="col" className="text-danger">Book Name</th>
-                <th scope="col" >Author Name</th>
-                <th scope="col" className="text-danger">Quantity</th>
-                <th scope="col" >price</th>
-            </tr>
-        </thead>
-            {
-                order.length > 0 ? order.map(singleOrder => 
-                <tbody>
-                    <tr>
-                        <th scope="row">{singleOrder.name}</th>
-                        <th scope="row">{singleOrder.authorName}</th>
-                        <td>1</td>
-                        <td>${singleOrder.price}</td>
-                    </tr>
-                </tbody>) :
-                <div class="d-flex justify-content-center">
-                    <div class="spinner-border text-danger" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                    </div>
-                </div>    
-            }
-          </table>
+            <div className="row">
+                <div className="col-md-12">
+                    <table className="table mb-5">
+                        <thead>
+                            <tr>
+                                <th scope="col" className="text-danger">Book Name</th>
+                                <th scope="col" >Author Name</th>
+                                <th scope="col" className="text-danger">Quantity</th>
+                                <th scope="col" >price</th>
+                            </tr>
+                        </thead>
+                        {
+                            order.length > 0 ? order.map(singleOrder =>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">{singleOrder.name}</th>
+                                        <th scope="row">{singleOrder.authorName}</th>
+                                        <td>1</td>
+                                        <td>${singleOrder.price}</td>
+                                    </tr>
+                                </tbody>) :
+                                <div class="d-flex justify-content-center">
+                                    <div class="spinner-border text-danger" role="status">
+                                        <span class="visually-hidden">Loading...</span>
+                                    </div>
+                                </div>
+                        }
+                    </table>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
     );
 };
 
